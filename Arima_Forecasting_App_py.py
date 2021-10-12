9ead866d182d2bc69061953097337af3acf26c57
Mounji-Salma <74664742+Mounji-Salma@users.noreply.github.com>
2021-10-12 19:47:14 +0100

    Add files via upload

--- Arima_Forecasting_App_py.py
+++ Arima_Forecasting_App_py.py
@@ -48,20 +48,24 @@ def auto_arima_for_df(df, trace: bool):
 
 # %%
 try:
-    sales = pd.read_csv('70prod_data.csv', index_col=0, parse_dates=True)
+    sales = pd.read_csv('https://github.com/Mounji-Salma/mirrored_Forecasting_Project/blob/main/70prod_data.csv', index_col=0, parse_dates=True)
     print("data imported")
 except FileNotFoundError:
     st.write("NotFoundError")
+    raise
 
 # %%
-joblib_Filename = "joblib_ARIMA_sales_Models.joblib"
+
+from urllib.request import urlopen
+#joblib_Filename = "joblib_ARIMA_sales_Models.joblib"
 
 # if compress: joblib_Filename += '.z'
 try:
-    models = joblib.load(joblib_Filename)
+    models = joblib.load(urlopen("https://raw.githubusercontent.com/Mounji-Salma/mirrored_Forecasting_Project/main/joblib_ARIMA_sales_Models.joblib"))
     print("models imported")
 except FileNotFoundError:
     st.write("NotFoundError")
+    raise
 
 
 # %%
